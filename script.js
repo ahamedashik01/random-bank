f95e130724caa782fddfc1e148663dd56596f458
ahamedashik01 <ashikshamim.007@gmail.com>
2021-08-16 04:21:50 +0600

    initial commit and html added

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('log-in').addEventListener('click', function () {
+    // get user email 
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    if (userEmail == 'ashikshamim@gamil.com' && userPassword == 'secret') {
+        window.location.href = 'bank.html';
+    }
+})
